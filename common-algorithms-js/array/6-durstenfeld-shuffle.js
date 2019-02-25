e8117d331649793dd60ed3a1af11e7b81175b419
Raghav Dinesh <root@ruwgxo.com>
2019-02-25 09:44:30 +0000

    Added Array 1-7 algorithms.

---
+++ common-algorithms-js/array/6-durstenfeld-shuffle.js
@@ -0,0 +1,19 @@
+/*
+ *   A JavaScript module which performs a Durstenfeld shuffle on a given array.
+ *   */
+
+'use strict'
+
+const durstenfeldShuffle = (input = null) => {
+	    if (input === null) return null
+	    let index = input.length
+	    while (index !== 0) {
+		            let shuffleIndex = Math.floor(Math.random() * index--)
+		            let swap = input[index]
+		            input[index] = input[shuffleIndex]
+		            input[shuffleIndex] = swap
+		        }
+	    return input
+}
+
+export default durstenfeldShuffle
---
+++ common-algorithms-js/array/7-gnome.sort.js
@@ -0,0 +1,20 @@
+/*
+ *   A JavaScript module which performs a gnome sort on a given array of numbers, ordering the elements from lowest to highest.
+ *   */
+
+'use strict'
+
+const gnomeSort = (array = null) => {
+	    if (array === null) return null
+	    let index = 0
+	    while (index < array.length) index === 0 || array[index - 1] <= array[index] ? index++ : swapIndicies(array, index--)
+	    return array
+}
+
+const swapIndicies = (array, index) => {
+	    let temp = array[index]
+	    array[index] = array[--index]
+	    array[index] = temp
+}
+
+export default gnomeSort
