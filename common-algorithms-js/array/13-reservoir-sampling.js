d9442ba0340c9d195a13865edfc674c35f65070e
Raghav Dinesh <root@ruwgxo.com>
2019-02-25 11:03:43 +0000

    Added array algorithms from 8 to 17

---
+++ common-algorithms-js/array/13-reservoir-sampling.js
@@ -0,0 +1,14 @@
+/*
+ *   A JavaScript module which randomly outputs a subset of an input array corresponding in length to the specified input integer using reservoir sampling.  Intended for large input arrays.
+ *   */
+
+'use strict'
+
+const reservoirSampling = (array = null, size = null) => {
+	    if (array === null || size === null) return null
+	    let output = array.slice(0, size)
+	    for (let i = size, j = Math.floor(Math.random() * (i - 2)) + 1; i < array.length; i++ , j = Math.floor(Math.random() * (i - 2)) + 1) if (j <= size) output[j] = array[i]
+	    return output
+}
+
+export default reservoirSampling
---
+++ common-algorithms-js/array/9-linear-search.js
@@ -0,0 +1,21 @@
+/*
+ *   A JavaScript module which performs a linear search for a given value inside an input array.  If the specified value is found, the index of the element in the array with that value is returned.  If the specified value is not found in the array null is returned.
+ *   */
+
+'use strict'
+
+const linearSearch = (array, value) => {
+	    let output = null
+	    if (typeof value === 'object') {
+		            value = JSON.stringify(value)
+		            for (let i = 0; i < array.length; i++) if (typeof array[i] === 'object') array[i] = JSON.stringify(array[i])
+		        }
+	    if (typeof value === 'function') {
+		            value = '' + value
+		            for (let i = 0; i < array.length; i++) if (typeof array[i] === 'function') array[i] = '' + array[i]
+		        }
+	    for (let i = 0; i < array.length; i++) if (array[i] === value) output = i
+	    return output
+}
+
+export default linearSearch
