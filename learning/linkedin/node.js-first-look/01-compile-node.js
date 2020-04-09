6e739af11554e8ad9e728b61b07ea952c7e788f6
Raghav Dinesh <root@ruwgxo.com>
2020-04-09 12:10:15 +0000

    Added learning from various platforms

---
+++ learning/linkedin/node.js-first-look/01-compile-node.js
@@ -0,0 +1,22 @@
+/*
+Compile Node.js
+
+> sudo su
+> cd /usr/local/src
+> cp [source file directory path] . # dot stands for pwd
+> gunzip [source file name]
+> ls
+> tar -xf [source tar file, to extract]
+> apt-get install build-essential libssl-dev
+> cd [name of folder]
+
+// to compile
+> ./configure
+> make
+> make install
+
+// to test 
+> node test.js
+
+# To update node in future, use nvm(Node Version Manager).
+*/
